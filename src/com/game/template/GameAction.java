bb57bb449a3250132884dad92a6510beb09c4afd
quemin2402 <mereyibraim2006@gmail.com>
2024-10-31 04:23:33 +0500

    a few changes

--- src/com/game/template/GameAction.java
+++ src/com/game/template/GameAction.java
@@ -3,13 +3,13 @@ package com.game.template;
 import com.game.Character;
 
 public abstract class GameAction {
-    public final void executeAction(Character attacker, Character target) {  // Добавляем target
+    public final void executeAction(Character attacker, Character target) {
         prepare();
         action(attacker, target);
         conclude();
     }
 
     protected abstract void prepare();
-    protected abstract void action(Character attacker, Character target);  // Изменяем метод action
+    protected abstract void action(Character attacker, Character target);
     protected abstract void conclude();
 }
